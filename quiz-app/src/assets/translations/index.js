f046dfe7c9103672e7e29c3f59d70ed45aea765b
CNiyomthum <chatchawan.niyomthum@gmail.com>
2022-06-11 17:37:03 +0700

    [Translation-th]: add Thai translation on Quizzs group-1

--- quiz-app/src/assets/translations/index.js
+++ quiz-app/src/assets/translations/index.js
@@ -1,10 +1,12 @@
 import englishQuizzes from './en/';
 import frenchQuizzes from './fr/';
 import spanishQuizzes from './es/';
+import thaiQuizzes from './th/';
 
 const messages = {
   en: englishQuizzes,
   fr: frenchQuizzes,
   es: spanishQuizzes,
+  th: thaiQuizzes,
 };
 export default messages;
---
+++ quiz-app/src/assets/translations/th/index.js
@@ -0,0 +1,21 @@
+import th0 from "./group-1.json";
+import th1 from "./group-2.json";
+import th2 from "./group-3.json";
+
+import th3 from "./group-4.json";
+
+import th4 from "./group-5.json";
+
+import th5 from "./group-6.json";
+
+
+const quiz = {
+  0: th0[0],
+  1: th1[0],
+  2: th2[0],
+  3: th3[0],
+  4: th4[0],
+  5: th5[0],
+};
+
+export default quiz;
